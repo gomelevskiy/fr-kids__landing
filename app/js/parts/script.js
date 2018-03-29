57f88b97446f71cb41c77f44ea6db46ca2620ffe
Алексей Гомелевский <aleksejgomelevskij@MacBook-Pro-Aleksej.local>
2018-03-29 11:54:52 +0300

    Mobile version

--- app/js/parts/script.js
+++ app/js/parts/script.js
@@ -11,3 +11,62 @@ $(document).on("scroll", function() {
     });
 
 });
+
+$(document).ready(function() {
+
+    // mask to phone
+    $('input[name="tel"]').mask('+7 (999) 999-99-99');
+
+    // Input as material design
+    $('.modal-input').blur(function(){
+        if($(this).val().length !== 0) {
+            $(this).addClass('filled');
+        }
+        else {
+            $(this).removeClass('filled');
+        }
+    });
+
+    // Parralax effect init
+    var rellax = new Rellax('.rellax');
+
+    // Modal
+    $(".btn-modal").click(function() {
+       $('.modal').modal("show");
+    });
+
+    $(".close-modal-btn").click(function() {
+        $('.modal').modal("hide");
+    });
+
+    // tabs init for Formats section
+    $(".tabs-formats .tabs-formats-points>a").click( function () {
+
+        $(".tabs-formats .tabs-formats-points>a").removeClass("current");
+        $(this).addClass("current");
+
+        $(".tabs-formats .tabs_content>div").hide();
+        t_content = $(this).attr("href");
+        $(t_content).show();
+
+        return false
+    });
+
+    $(".tabs-formats .tabs-formats-points>a:first").trigger("click");
+
+    // tabs init for Steps section
+    $(".tabs-steps>a").click( function () {
+
+        $(".tabs-steps>a").removeClass("current");
+        $(this).addClass("current");
+
+        $(".tabs-steps .tabs_content>div").hide();
+        t_content = $(this).attr("href");
+        $(t_content).show();
+
+        return false
+    });
+
+    $(".tabs-steps>a:first").trigger("click");
+
+});
--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,5 @@
 //= parts/jquery-3.3.1.min.js
 //= parts/bootstrap.min.js
+//= parts/jquery.maskedinput.min.js
+//= parts/rellax.min.js
 //= parts/script.js
